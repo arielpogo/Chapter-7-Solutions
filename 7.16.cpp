50a0a1ab934994c97b2b815b615ab6c8e56c3f67
ryebreadthrills <127140801+ryebreadthrills@users.noreply.github.com>
2023-08-10 18:32:47 -0400

    Update 7.16.cpp
    Expanded documentation

--- 7.16.cpp
+++ 7.16.cpp
@@ -5,23 +5,29 @@
 using namespace std;
 #define p 3 //the pivot where we do insertion sort
 
+/*
+Question from the book:
+7.16 How would you implement mergesort without using recursion?
+*/
+
+//This is actually selection sort, I didn't realize until I was far into all of these. Selection sort is always O(N^2), unlike actual insertion sort.
 void insertionSort(vector<int>& v) {
 	int lowestIndex = 0;
 	
 	for (int i = 0; i < v.size(); i++) {
 		lowestIndex = i;
 		for (int j = i; j < v.size(); j++) {
-			if (v[j] < v[lowestIndex]) lowestIndex = j;
+			if (v[j] < v[lowestIndex]) lowestIndex = j; //find the index with the lowest value, between i and the end
 		}
-		swap(v[i], v[lowestIndex]);
+		swap(v[i], v[lowestIndex]); //swap
 	}
 }
 
 void mergeSort(vector<int>& v) {
 	vector<vector<int>> splitV; //a vector of the original vector split up
 
-	//split the vector so each smaller vector is size p
-	for (int i = 0; i < 1+((v.size()-1) / p); i++) { //integer ciel division, so 10/3 = 4, 10/4 = 3 etc.
+	//split the original vector so each smaller vector is size p
+	for (int i = 0; i < 1+((v.size()-1) / p); i++) { //integer ciel division, so 10/3 = 4, for example 
 		vector<int> temp;
 		for (int j = i*p; j < i*p+p && j < v.size(); j++) {
 			temp.push_back(v[j]);
@@ -32,10 +38,10 @@ void mergeSort(vector<int>& v) {
 	//sort each smaller chunk
 	for (int i = 0; i < splitV.size(); i++) insertionSort(splitV[i]);
 
-	//merge by taking the last two
+	//merge the last two vectors until only one is left
 	vector<int> temp;
 	while (splitV.size() > 1) {
-		int svs = splitV.size(); //splitV size
+		int svs = splitV.size(); //splitV's size
 		int stl = 0; //second to last vector iterator
 		int stl_max = splitV[svs - 2].size(); //stl's limit, the size of the second-to-last vector
 		int l = 0; //last vector iterator
